ec305022a2e4e0f84bc22a0b321e480df60423e3
Bryce Lovell <40742705+BryceWDesign@users.noreply.github.com>
2026-06-06 18:13:53 -0700

    Update import and function signature in test file

--- tests/test_intent_loop_handoff.py
+++ tests/test_intent_loop_handoff.py
@@ -11,11 +11,11 @@ from ix_function.intent_loop_handoff import (
     validate_intent_loop_handoff_packet,
 )
 from ix_function.observation import MeasuredValue, OutcomeRecord
-from ix_function.trial import TransferTrialInput, run_transfer_trial
+from ix_function.trial import TransferTrialInput, TransferTrialResult, run_transfer_trial
 from tests.fixtures import make_trial_input
 
 
-def make_failed_result():
+def make_failed_result() -> TransferTrialResult:
     trial_input = make_trial_input()
     failed_input = TransferTrialInput(
         trial_id=trial_input.trial_id,
